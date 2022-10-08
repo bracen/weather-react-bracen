7f1d7253f0a0b0d3b3ffdd1fa09e79e974986ec5
bracen <ninashindesign@gmail.com>
2022-10-08 15:26:05 +1300

    styling css

--- src/Weather.js
+++ src/Weather.js
@@ -15,67 +15,63 @@ export default function Weather() {
     maxTemperature: 18,
   };
   return (
-      <div className="weather">
-        <div className="card-body">
-          <form className="search text-center">
-            <input
-              className="search-input"
-              type="text"
-              placeholder="🔎 serach for a city"
-            />
-            <input type="submit" value="search" />
-          </form>
-          <div className="container w-90 small p-4 my-4 border">
-            <div className="row city">
-              <div className="col-sm pt-4 text-center">
-                <span className="city">{weatherData.city}</span>
-                <br />
-                <ul>
-                  <li>{weatherData.date}</li>
-                  <li>
-                    <span className="description">
-                      {weatherData.description}
-                    </span>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-sm">
-                <span className="weahter-icon">
-                  <img
-                    src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
-                    alt="Sunny"
-                  />
-                </span>
-                <br />
-                <ul>
-                  <li>
-                    {" "}
-                    L: <span>{weatherData.minTemperature}</span>° | H:{" "}
-                    <span>{weatherData.maxTemperature}</span>°{" "}
-                  </li>
-                </ul>
-              </div>
-              <div class="col-sm pt-4">
-                <ul>
-                  <li>
-                    <span className="temperature">
-                      {weatherData.temperature}
-                    </span>
-                    <span className="celsius">℃</span>
-                  </li>
-                  <li>
-                    Wind: <span>{weatherData.wind}</span> km/h
-                  </li>
-                  <li>
-                    Humidity: <span>{weatherData.humidity}</span> %
-                  </li>
-                </ul>
-              </div>
+    <div className="weather">
+      <div className="card-body">
+        <form className="search text-center">
+          <input
+            className="search-input"
+            type="text"
+            placeholder="🔎 serach for a city"
+            autoFocus="on"
+          />
+          <input type="submit" value="search" />
+        </form>
+        <div className="container w-90 small p-4 my-4 border">
+          <div className="row city">
+            <div className="col-sm pt-4 text-center">
+              <span className="city">{weatherData.city}</span>
+              <br />
+              <ul>
+                <li>{weatherData.date}</li>
+                <li>
+                  <span className="description">{weatherData.description}</span>
+                </li>
+              </ul>
+            </div>
+            <div className="col-sm">
+              <span className="weahter-icon">
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
+                  alt="Sunny"
+                />
+              </span>
+              <br />
+              <ul>
+                <li>
+                  {" "}
+                  <span className="low-high-temp">L: {weatherData.minTemperature}° | H:{" "}
+                  {weatherData.maxTemperature}</span>°{" "}
+                </li>
+              </ul>
+            </div>
+            <div class="col-sm pt-4">
+              <ul>
+                <li>
+                  <span className="temperature">{weatherData.temperature}</span>
+                  <span className="celsius">℃</span>
+                </li>
+                <li>
+                  Wind: <span>{weatherData.wind}</span> km/h
+                </li>
+                <li>
+                  Humidity: <span>{weatherData.humidity}</span> %
+                </li>
+              </ul>
             </div>
           </div>
-          <div className="forecast text-center px-5 pt-2"></div>
         </div>
+        <div className="forecast text-center px-5 pt-2"></div>
       </div>
-     
+    </div>
   );
 }
